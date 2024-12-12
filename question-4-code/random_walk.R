778bf77f5caf44265c4d8279b329ad9371f736cc
Freya Cook <freyacook@outlook.com>
2024-12-12 14:49:24 +0000

    Edited for reproducibility

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -10,17 +10,22 @@ random_walk  <- function (n_steps) {
   
   df[1,] <- c(0,0,1)
   
+  set.seed(73) #sets random seed for whole of the for loop 
+  #if set.seed was within the for loop, each iteration of the loop would give the same result
+    #i.e every angle generated would be the same (i.e. determined by the same number in the pseudorandom sequence denoted by set.seed(73))
+  #having set.seed outside of the loop means that the angles are drawn from the same pseudorandom sequence (so are reproducible), but do not have the same value for each iteration
+  
   for (i in 2:n_steps) {
     
-    h <- 0.25
+    h <- 0.25 
     
-    angle <- runif(1, min = 0, max = 2*pi)
+    angle <- runif(1, min = 0, max = 2*pi) #selects 1 random value between 0 and 360 to be angle
     
-    df[i,1] <- df[i-1,1] + cos(angle)*h
+    df[i,1] <- df[i-1,1] + cos(angle)*h #determines x value, relative to x in last coordinate/ step 
     
-    df[i,2] <- df[i-1,2] + sin(angle)*h
+    df[i,2] <- df[i-1,2] + sin(angle)*h #determines y value, relative to y in last coordinate/ step
     
-    df[i,3] <- i
+    df[i,3] <- i #documents the current step, enables tracking & plotting of time of each step 
     
   }
   
